71d6cb0bf1fbaf86bd6360332feea4e7df7897ee
Jennga <jengaestudios@gmail.com>
2025-06-02 13:05:49 +0200

    quite la configuracion para gitpages

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    output: 'export',
-    images: {
-      unoptimized: true,
-    },
-    trailingSlash: true,
-    basePath: '/porfolio', // porque tu repo no es jennga.github.io
+   
 };
 
 export default nextConfig;
