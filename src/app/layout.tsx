fb4c95811d6aa36d54e1b36fc44f35026f805b71
Jennga <jengaestudios@gmail.com>
2025-06-03 20:32:28 +0200

    fix tailwind problems

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,7 @@
 import type { Metadata } from "next";
-//import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-//const geistSans = Geist({
-  //variable: "--font-geist-sans",
-  //subsets: ["latin"],
-//});
-
-//const geistMono = Geist_Mono({
-  //variable: "--font-geist-mono",
-  //subsets: ["latin"],
-//});
-
 export const metadata: Metadata = {
-  title: "Porfolio Jennga",
+  title: "Portfolio Jennga",
   description: "Lugar para la ciberseguridad",
 };
 
@@ -23,10 +11,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`antialiased`}
-      >
+    <html lang="es">
+      <body>
         {children}
       </body>
     </html>
