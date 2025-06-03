807a125223a68238c87bd5561f300b18d5025535
Jennga <jengaestudios@gmail.com>
2025-06-03 21:07:08 +0200

    correcc

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 export const metadata: Metadata = {
-  title: "Portfolio Jennga",
+  title: "Porfolio Jennga",
   description: "Lugar para la ciberseguridad",
 };
 
