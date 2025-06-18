861a5783ce03e88b1e517e6b6e8f03fbea3af8e2
Jennga <jengaestudios@gmail.com>
2025-06-18 14:41:28 +0200

    me falto el build

--- src/app/menu/cyber/Analisis.tsx
+++ src/app/menu/cyber/Analisis.tsx
@@ -6,7 +6,8 @@ export default function Analisis() {
             <h1 className={styles.h1todos}>Análisis Forense</h1>
             <div className={styles.introduccion}>El análisis forense en ciberseguridad es el proceso de identificar, recopilar, preservar y analizar evidencia digital para investigar incidentes de seguridad y determinar su causa, impacto y responsables.</div>
             <div>
-                <div></div>
+                <div>
+                </div>
                 <div></div>
                 <div></div>
             </div>
